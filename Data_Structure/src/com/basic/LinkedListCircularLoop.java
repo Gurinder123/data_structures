93174aa7a1fb4681c5052088abf01bcdda0af3be
Gurinder singh <gurindersingh@xebia.com>
2016-07-24 14:33:21 +0530

    adding circular loop

---
+++ Data_Structure/src/com/basic/LinkedListCircularLoop.java
@@ -0,0 +1,38 @@
+package com.basic;
+
+/**
+ * Created by gurinder on 24/7/16.
+ */
+public class LinkedListCircularLoop {
+    public static void main(String[] args) {
+        Node<Integer> node = new Node(1, null);
+        findloop(node);
+    }
+
+    private static void findloop(Node<Integer> node) {
+        Node slow = node;
+        Node faster = node.next;
+
+        while (true) {
+            if (faster == null || faster.next == null)
+                return;
+            else if (faster == slow || faster.next == slow)
+                System.out.println("Loop exist");
+            else {
+                slow = slow.next;
+                faster = faster.next;
+            }
+        }
+    }
+}
+
+
+class Node<V> {
+    Node<V> next;
+    V data;
+
+    public Node(V data, Node<V> next) {
+        this.next = next;
+        this.data = data;
+    }
+}
