e423755e599f980f60d1061e4408589963724303
Gurinder Singh <gurinder@theinnovationinc.co>
2018-03-17 17:53:07 +0530

    Update UrlShortener.java
    changing logic

--- Data_Structure/src/com/basic/UrlShortener.java
+++ Data_Structure/src/com/basic/UrlShortener.java
@@ -4,34 +4,51 @@ package com.basic;
  * Created by Gurinder on 7/8/2016.
  */
 public class UrlShortener {
-    public String idToTinyUrl(int id)
-    {
-        String map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder tinyUrl = new StringBuilder();
-        while(id>0)
-        {
-            tinyUrl.append(map.charAt(id%62));
-            id /= 62;
-        }
-        return tinyUrl.reverse().toString();
+
+    private static String key = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    private static  Map<Long, String> urlMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        String url = "https://docs.spring.io/spring/docs/2.5.x/reference/aop.html";
+
+
+        long id = 12345;
+
+        urlMap.put(id, url);
+
+        String shortUrl = idToShortUrl(id);
+
+        id = shortUrlToId(shortUrl);
+
+        System.out.println(urlMap.get(id));
     }
 
-    public int tinyUrlToId(String tinyUrl)
-    {
-        int id = 0;
-        String map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        for(int i=0;i<tinyUrl.length();i++)
-        {
-            id = (id*62) + map.indexOf(tinyUrl.charAt(i));
+    private static long shortUrlToId(String url) {
+        long hash = 0;
+
+        for (int i = 0; i < url.length(); i++) {
+            {
+                if ('a' <= url.charAt(i) && url.charAt(i) <= 'z')
+                    hash = hash * 62 + url.charAt(i) - 'a';
+                if ('A' <= url.charAt(i) && url.charAt(i) <= 'Z')
+                    hash = hash * 62 + url.charAt(i) - 'A' + 26;
+                if ('0' <= url.charAt(i) && url.charAt(i) <= '9')
+                    hash = hash * 62 + url.charAt(i) - '0' + 52;
+            }
         }
-        return id;
+
+        return hash;
     }
 
-    public static void main(String args[])
-    {
-        int id = 12345;
-        UrlShortener t = new UrlShortener();
-        System.out.println(t.idToTinyUrl(id));
-        System.out.println(t.tinyUrlToId("dnh"));
+    private static String idToShortUrl(long id) {
+        StringBuilder builder = new StringBuilder();
+
+        while (id > 0) {
+            builder.append(key.charAt((int)id % 62));
+            id = id / 62;
+        }
+
+        return builder.reverse().toString();
     }
 }
